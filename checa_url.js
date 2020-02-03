934dd8afb76a2ba996a04858678503df173205ed
Diego Perez <diegoperez.ufes@gmail.com>
2020-02-03 10:06:37 -0300

    Popup dinamico
    Extensão com os popups separados por alguns sites para exemplificar.

--- checa_url.js
+++ checa_url.js
@@ -5,19 +5,23 @@ chrome.tabs.onUpdated.addListener( function obten_url(){
             console.log(url);
             if (url.includes("kabum.com.br")){
                 chrome.browserAction.setIcon({path:'/imgs/icon_green.png'});
-                chrome.browserAction.setPopup({popup:'/imgs/kabum_teste.png'})
+                chrome.browserAction.setPopup({popup:''});
             }
             else if (url.includes("magazineluiza.com.br")){
                 chrome.browserAction.setIcon({path:'/imgs/icon_green.png'});
+                chrome.browserAction.setPopup({popup:''});
             }
             else if (url.includes("submarino.com.br")){
                 chrome.browserAction.setIcon({path:'/imgs/icon_blue.png'});
+                chrome.browserAction.setPopup({popup:''});
             }
             else if (url.includes("americanas.com")){
                 chrome.browserAction.setIcon({path:'/imgs/icon_red.png'});
+                chrome.browserAction.setPopup({popup:''});
             }
             else if(url.includes("amazon.com")){
                 chrome.browserAction.setIcon({path:'/imgs/icon_yellow.png'});
+                chrome.browserAction.setPopup({popup:''});
             }
             else{
                 chrome.browserAction.setIcon({path:'/imgs/icon_gray.png'});
